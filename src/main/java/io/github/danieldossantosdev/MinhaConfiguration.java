5539fd26b61056e1020cb98a90d8a2f87d3ee121
Daniel Santos <daniel.nascimento@ul.com>
2023-07-11 06:36:30 -0300

    Trabalhando com multiplos ambientes e profiles

--- src/main/java/io/github/danieldossantosdev/MinhaConfiguration.java
+++ src/main/java/io/github/danieldossantosdev/MinhaConfiguration.java
@@ -1,7 +1,16 @@
 package io.github.danieldossantosdev;
 
-import org.springframework.context.annotation.Configuration;
-@Configuration
-public class MinhaConfiguration {
+import io.github.danieldossantosdev.service.Development;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
+
+@Development
+public class MinhaConfiguration {
+    @Bean
+    public CommandLineRunner executar (){
+        return args -> {
+    System.out.println("Rodando a Configuração de desenvolvimento");
+        };
+    }
 }
